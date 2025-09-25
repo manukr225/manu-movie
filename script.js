657d3c69c2f9b85a8f413564db663d679653f80a
manukr225 <voemanu225@gmail.com>
2025-09-25 10:46:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let allMovies = [];
-
-fetch('data/movies.json')
-  .then(res => res.json())
-  .then(movies => {
-    allMovies = movies;
-    renderMovies(movies);
-  });
-
 function renderMovies(movies) {
   const grid = document.getElementById('movieGrid');
   grid.innerHTML = '';
@@ -14,64 +5,37 @@ function renderMovies(movies) {
   movies.forEach((movie, index) => {
     const card = document.createElement('div');
     card.className = 'card';
+    let videoSection = '';
 
-    if (movie.format === 'iframe') {
-      card.innerHTML = `
-        <a href="movie.html?id=${movie.id}">
-          <img src="${movie.poster}" alt="${movie.title}">
-          <div class="card-title">${movie.title}</div>
-        </a>
-        <iframe
-          src="${movie.embed}"
-          width="320"
-          height="180"
-          frameborder="0"
-          allowfullscreen
-          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-        ></iframe>
-      `;
-    } else if (movie.format === 'videojs') {
-      card.innerHTML = `
-        <a href="movie.html?id=${movie.id}">
-          <img src="${movie.poster}" alt="${movie.title}">
-          <div class="card-title">${movie.title}</div>
-        </a>
-        <video id="video-${index}" controls width="320" height="180"></video>
-      `;
-    } else {
-      card.innerHTML = `
-        <a href="movie.html?id=${movie.id}">
-          <img src="${movie.poster}" alt="${movie.title}">
-          <div class="card-title">${movie.title}</div>
-        </a>
-      `;
+    // HLS STREAM CARD
+    if (movie.format === 'videojs') {
+      videoSection = `<video id="video-${index}" controls width="320" height="180"></video>`;
+    }
+    // IFRAME EMBED CARD
+    else if (movie.format === 'iframe') {
+      videoSection = `<iframe src="${movie.embed}" width="320" height="180" frameborder="0"
+      allowfullscreen sandbox="allow-same-origin allow-scripts allow-popups"></iframe>`;
     }
 
+    card.innerHTML = `
+      <a href="movie.html?id=${movie.id}">
+        <img src="${movie.poster}" alt="${movie.title}">
+        <div class="card-title">${movie.title}</div>
+      </a>
+      ${videoSection}
+    `;
     grid.appendChild(card);
 
+    // Only run hls.js for HLS videos
     if (movie.format === 'videojs') {
       var video = document.getElementById(`video-${index}`);
       if (Hls.isSupported()) {
         var hls = new Hls();
         hls.loadSource(movie.embed);
         hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {});
       } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
         video.src = movie.embed;
       }
     }
   });
-}
-
-document.getElementById('searchInput').addEventListener('input', filterMovies);
-document.getElementById('categoryFilter').addEventListener('change', filterMovies);
-
-function filterMovies() {
-  const query = document.getElementById('searchInput').value.toLowerCase();
-  const category = document.getElementById('categoryFilter').value;
-  const filtered = allMovies.filter(movie =>
-    movie.title.toLowerCase().includes(query) &&
-    (category === '' || movie.category === category)
-  );
-  renderMovies(filtered);
 }
