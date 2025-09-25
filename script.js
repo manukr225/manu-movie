19e083245dc59ff8eb68a5848fbe17cff42d053a
manukr225 <voemanu225@gmail.com>
2025-09-25 10:41:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,56 @@ fetch('data/movies.json')
 function renderMovies(movies) {
   const grid = document.getElementById('movieGrid');
   grid.innerHTML = '';
-  movies.forEach(movie => {
+
+  movies.forEach((movie, index) => {
     const card = document.createElement('div');
     card.className = 'card';
-    card.innerHTML = `
-      <a href="movie.html?id=${movie.id}">
-        <img src="${movie.poster}" alt="${movie.title}">
-        <div class="card-title">${movie.title}</div>
-      </a>
-    `;
+
+    if (movie.format === 'iframe') {
+      card.innerHTML = `
+        <a href="movie.html?id=${movie.id}">
+          <img src="${movie.poster}" alt="${movie.title}">
+          <div class="card-title">${movie.title}</div>
+        </a>
+        <iframe
+          src="${movie.embed}"
+          width="320"
+          height="180"
+          frameborder="0"
+          allowfullscreen
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+        ></iframe>
+      `;
+    } else if (movie.format === 'videojs') {
+      card.innerHTML = `
+        <a href="movie.html?id=${movie.id}">
+          <img src="${movie.poster}" alt="${movie.title}">
+          <div class="card-title">${movie.title}</div>
+        </a>
+        <video id="video-${index}" controls width="320" height="180"></video>
+      `;
+    } else {
+      card.innerHTML = `
+        <a href="movie.html?id=${movie.id}">
+          <img src="${movie.poster}" alt="${movie.title}">
+          <div class="card-title">${movie.title}</div>
+        </a>
+      `;
+    }
+
     grid.appendChild(card);
+
+    if (movie.format === 'videojs') {
+      var video = document.getElementById(`video-${index}`);
+      if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(movie.embed);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {});
+      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = movie.embed;
+      }
+    }
   });
 }
 
